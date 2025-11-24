3c67c42655464b3135990fe341a0fd046ff78245
lxj901 <yeli13521674058@163.com>
2025-11-24 14:09:30 +0800

    Integrate React Native via CocoaPods workspace

---
+++ qinghe/Pods/Target Support Files/Pods-qinghe-qingheTests/Pods-qinghe-qingheTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_qinghe_qingheTests : NSObject
+@end
+@implementation PodsDummy_Pods_qinghe_qingheTests
+@end
---
+++ qinghe/Pods/Target Support Files/Pods-qinghe/Pods-qinghe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_qinghe : NSObject
+@end
+@implementation PodsDummy_Pods_qinghe
+@end
